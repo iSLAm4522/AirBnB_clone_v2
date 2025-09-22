aa869cbad92604aaad6c1d4c977bd5f127ac992a
iSLAm4522 <islammadian798@gmail.com>
2025-09-22 13:42:58 +0300

    Add 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A simple Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """Returns 'C ' followed by the value of the text variable
+    (replace underscore _ symbols with a space )"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
